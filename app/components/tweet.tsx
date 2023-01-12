ec1c6f23818c0c32746703cb1d1f6a377e1f2c1d
pm3512 <aobolens@andrew.cmu.edu>
2023-01-12 05:31:01 -0500

    finished styling for tweet component

--- app/components/tweet.tsx
+++ app/components/tweet.tsx
@@ -7,6 +7,9 @@ export default function TweetComponent(props: Tweet) {
       <div>
         <h3 className="inline font-semibold">
           {/*TODO: Link to author page or the profile page if it's our own tweet.*/}
+          <Link href={'/'}>
+            {props.author_display}
+          </Link>
         </h3>
         <span className="text-gray-500"> • </span>
         <time className="text-gray-500 text-sm" title={props.created_at}>
@@ -18,7 +21,14 @@ export default function TweetComponent(props: Tweet) {
       </div>
       <p className="whitespace-pre-wrap">{props.content}</p>
       {/*TODO: Link to the topic page.*/}
-      {props.topic ? <Link href={"/"}>{props.topic}</Link> : null}
+      {props.topic ? (
+        <Link
+          href={"/"}
+          className="inline-block mt-2 text-pink-500 hover:underline"
+        >
+          #{props.topic}
+        </Link>
+      ) : null}
     </div>
   );
 }
